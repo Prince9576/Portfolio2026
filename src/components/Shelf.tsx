b05795896105085f0258638bf5ad79304b36d78e
Prince Kumar <princekumar@Mac.lan>
2026-02-13 23:34:07 +0530

    Optimization I : Shelf

--- src/components/Shelf.tsx
+++ src/components/Shelf.tsx
@@ -1,115 +1,87 @@
 import { useGLTF } from "@react-three/drei";
 import { memo, type JSX } from "react";
-import * as THREE from "three";
-import type { GLTF } from "three-stdlib";
 
 
 const Shelf = memo(function Shelf(props: JSX.IntrinsicElements["group"]) {
-  const { nodes, materials } = useGLTF("/models/Shelf.glb") as any;
+  const { nodes, materials } = useGLTF("/models/shelf-draco-v1.glb") as any;
   return (
     <group {...props} dispose={null}>
-      <group rotation={[-Math.PI / 2, 0, 0]} scale={0.45}>
-        <group rotation={[Math.PI / 2, 0, 0]} scale={0.01}>
-          <group rotation={[-Math.PI / 2, 0, 0]} scale={100}>
-            <group position={[0.302, -0.282, 5.363]} scale={0.294}>
-              <mesh
-                geometry={nodes.Side_Panel_Panels_0_1.geometry}
-                material={materials.Panels}
-              />
-              <mesh
-                geometry={nodes.Side_Panel_Panels_0_2.geometry}
-                material={materials.Mesh}
-              />
-              <mesh
-                geometry={nodes.Side_Panel_Panels_0_3.geometry}
-                material={materials.Engine}
-              />
-              <mesh
-                geometry={nodes.Side_Panel_Panels_0_4.geometry}
-                material={materials.Decal}
-              />
-            </group>
-          </group>
-        </group>
-      </group>
-      <group
-        position={[0.058, 1.639, -0.945]}
-        rotation={[-1.571, 0.002, 1.603]}
-        scale={0.462}
-      >
-        <mesh
-          geometry={nodes.Object_2.geometry}
-          material={materials["nrt_eye.png.001_nrt_eye.png"]}
-        />
-        <mesh
-          geometry={nodes.Object_3.geometry}
-          material={materials["nrt_tex01.png_nrt_tex01.png"]}
-        />
-        <mesh
-          geometry={nodes.Object_4.geometry}
-          material={materials["nrt_tex02.png_nrt_tex02.png"]}
-        />
-        <mesh
-          geometry={nodes.Object_5.geometry}
-          material={materials["nrta_tex2.png_nrt_tex02.png"]}
-        />
-      </group>
-      <group
-        position={[0.034, 1.448, 0.084]}
-        rotation={[-Math.PI, 1.504, -Math.PI]}
-        scale={0.222}
-      >
+      <group position={[0.034, 1.448, 0.084]} rotation={[-Math.PI, 1.504, -Math.PI]} scale={0.222}>
         <mesh
           geometry={nodes.Sphere001.geometry}
-          material={materials["Material.002"]}
+          material={materials['Material.002']}
         />
         <mesh
           geometry={nodes.Sphere001_1.geometry}
-          material={materials["Material.001"]}
+          material={materials.PaletteMaterial001}
         />
       </group>
       <group
         position={[0.05, 2.777, 0.121]}
         rotation={[-Math.PI, 0, -Math.PI]}
-        scale={[-0.491, -0.05, -0.318]}
-      >
+        scale={[-0.491, -0.05, -0.318]}>
         <mesh
           geometry={nodes.Cube006.geometry}
-          material={materials["Material.003"]}
+          material={materials.PaletteMaterial003}
         />
         <mesh
           geometry={nodes.Cube006_1.geometry}
-          material={materials.Neon_bike}
+          material={materials.PaletteMaterial002}
         />
       </group>
-      <group
+      <mesh
+        geometry={nodes.Shelf2001.geometry}
+        material={materials.PaletteMaterial004}
         position={[0.05, 1.573, 0.121]}
         rotation={[-Math.PI, 0, -Math.PI]}
         scale={[-0.491, -0.05, -0.318]}
-      >
-        <mesh
-          geometry={nodes.Cube005.geometry}
-          material={materials["Material.003"]}
-        />
-        <mesh
-          geometry={nodes.Cube005_1.geometry}
-          material={materials.Neon_edge_3}
-        />
-      </group>
-      <group
+      />
+      <mesh
+        geometry={nodes.Shelf3001.geometry}
+        material={materials.PaletteMaterial005}
         position={[0.05, 2.175, -0.942]}
         rotation={[-Math.PI, 0, -Math.PI]}
         scale={[-0.491, -0.05, -0.318]}
-      >
+      />
+      <group position={[0.136, 2.412, 0.127]} rotation={[-Math.PI / 2, 0, 0]} scale={0.132}>
         <mesh
-          geometry={nodes.Cube004.geometry}
-          material={materials["Material.003"]}
+          geometry={nodes.Side_Panel_Panels_0_1.geometry}
+          material={materials.Panels}
         />
         <mesh
-          geometry={nodes.Cube004_1.geometry}
-          material={materials.Neon_edge_2}
+          geometry={nodes.Side_Panel_Panels_0_2.geometry}
+          material={materials.Mesh}
+        />
+        <mesh
+          geometry={nodes.Side_Panel_Panels_0_3.geometry}
+          material={materials.Engine}
+        />
+        <mesh
+          geometry={nodes.Side_Panel_Panels_0_4.geometry}
+          material={materials.Decal}
         />
       </group>
+      <mesh
+        geometry={nodes.Object_2.geometry}
+        material={materials['nrt_eye.png.001_nrt_eye.png']}
+        position={[0.058, 1.639, -0.945]}
+        rotation={[-1.571, 0.002, 1.603]}
+        scale={0.462}
+      />
+      <mesh
+        geometry={nodes.Object_3.geometry}
+        material={materials['nrt_tex01.png_nrt_tex01.png']}
+        position={[0.058, 1.639, -0.945]}
+        rotation={[-1.571, 0.002, 1.603]}
+        scale={0.462}
+      />
+      <mesh
+        geometry={nodes.Object_4.geometry}
+        material={materials['nrt_tex02.png_nrt_tex02.png']}
+        position={[0.058, 1.639, -0.945]}
+        rotation={[-1.571, 0.002, 1.603]}
+        scale={0.462}
+      />
     </group>
   );
 });
@@ -118,4 +90,4 @@ Shelf.displayName = "Shelf";
 
 export default Shelf;
 
-useGLTF.preload("/models/Shelf.glb", true);
+useGLTF.preload("/models/shelf-draco-v1.glb", true);
